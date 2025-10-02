86154625268eb93a741b9a9ab8414c3a3a306248
Jaume <mortejauroldan@gmail.com>
2025-10-02 18:15:42 +0200

    Cambios en el token

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Navigate, Route, Routes, useNavigate} from 'react-router
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Artistas from '../pages/Artistas';
+import Home from '../pages/Home'
 const Router = ({auth}) => {
     return <BrowserRouter>
         <Routes>
             <Route index element={<Login/>} />
             <Route path="/Register" element={<Register/>} />
              <Route path="/Artistas" element={auth ? <Artistas/> : <Navigate to="/"/>} />
+             <Route path="/Home" element={auth ? <Home/> : <Navigate to="/"/>} />
             <Route path="*" element={<div>404</div>} />
         </Routes>
 </BrowserRouter>
