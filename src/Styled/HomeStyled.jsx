bc1de10eafcbffeed60a3f8c5fb45447741fba14
Jaume <mortejauroldan@gmail.com>
2025-10-06 20:11:54 +0200

    Styled Components pt 1

---
+++ src/Styled/HomeStyled.jsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Main = styled.main `
+  padding: 2rem;
+
+`
+export const Title = styled.h1 `
+  background-color: var(--primary-bg);
+  color: var(--text-color);
+  border-radius: 30px;
+  width: fit-content;
+  text-align: center;
+  margin: 80px auto 2rem auto;
+  font-size: 2.5em;
+  text-shadow: 2px 2px 4px #000000;
+  padding: 0.5rem 1.5rem;
+`
