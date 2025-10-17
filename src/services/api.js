07e745bf708e1870f91a4eeaeac4b0409ef13118
Jaume <mortejauroldan@gmail.com>
2025-10-17 15:35:43 +0200

    Railway config

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
- const api = axios.create({ baseURL: 'http://localhost:8082' });
+ const api = axios.create({ baseURL: 'https://artenaturalback-production.up.railway.app' });
 
  export default api
