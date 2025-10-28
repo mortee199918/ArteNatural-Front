241cbfbeaab0fbc89da267217337dc9d2cc01b68
KevinMS98 <kevinmallque3010@gmail.com>
2025-10-28 16:37:24 +0100

    checkout update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-  export const apiurl = 'https://artenaturalback-production.up.railway.app'
-  //export const apiurl = 'localhost:8082'
- const api = axios.create({ baseURL: apiurl });
-  
+export const apiurl = 'https://artenaturalback-production.up.railway.app'
+//export const apiurl = 'localhost:8082'
+const api = axios.create({ baseURL: apiurl });
 
- export default api
+
+export default api
