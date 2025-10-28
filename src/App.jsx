6347af30779103c34d7d646a042980f9b56844c5
Alex diaz <mrplanning2@gmail.com>
2025-10-28 17:02:37 +0100

    Unauthenticated requests fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import TokenProvider from "./providers/TokenProvider";
 import { unsetToken, setAuth, validateToken } from "./services/auth";
 import { useState, useEffect } from "react";
 import { CartProvider } from "./pages/CartContext";
+import { AuthProvider } from "./providers/AuthProvider";
 
 const App = () => {
 
 
   return (
     <TokenProvider>
-      <CartProvider> {/* ✅ Envuelve Router con CartProvider */}
-        <Router />
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider> {/* ✅ Envuelve Router con CartProvider */}
+          <Router />
+        </CartProvider>
+      </AuthProvider>
     </TokenProvider>
   );
 }
